e2eda9434d6e07084ab27a054ac69f82a6a9d289
Ellsy23 <dlwnsgus3153@naver.com>
2023-10-11 15:10:03 +0900

    feat: update app.py;

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__, static_folder="build/static", template_folder="build")
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+
 @app.route("/logo.png")
 def logo():
-    return Flask.send_static_file("logo.png")
+    return send_file('build/static/logo.png')
 
-print('Starting Flask!')
 
-app.debug=True
-app.run(host= "0.0.0.0", port=5001)
+print('Running EUTCHA Serving Application')
+
+
+app.run(host="0.0.0.0", port=5001, debug=True)
